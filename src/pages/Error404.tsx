ef486f7fbbbb83b934ee49922810c95dd6fe930d
Aleksander Baska <baskaaleksander03@gmail.com>
2024-02-07 23:05:41 +0100

    added 404 error page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,7 +1,10 @@
 
 const Error404 = () => {
   return (
-    <div>404</div>
+    <div className="h-screen bg-grey-08 text-white-0 flex justify-center items-center flex-col">
+      <h1 className="text-6xl font-bold text-center">404</h1>
+      <h2 className="text-2xl text-center">Page Not Found</h2>
+    </div>
   )
 }
 
